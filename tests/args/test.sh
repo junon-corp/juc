8a4acd4f59742f8d3d70d51f1fa9af083d147d2d
antoninhrlt <antoninhrlt1@gmail.com>
2022-03-20 16:05:25 +0100

    Test for args

--- tests/args/test.sh
+++ tests/args/test.sh
@@ -9,7 +9,7 @@ BIN=build/debug/juc
 I=1
 
 function print_test_name() {
-    echo "--- Test" $I "---"
+    echo "---------------> Test" $I
     ((I++))
 }
 
@@ -17,10 +17,23 @@ print_test_name
 $BIN
 
 print_test_name
-$BIN tests/args/file.ju
-
-print_test_name
 $BIN -h
 
 print_test_name
 $BIN -z
+
+print_test_name
+$BIN -d khazmfla/
+
+print_test_name
+$BIN -p AzR
+
+# these commands should not fail
+print_test_name
+$BIN -p Windows
+
+print_test_name
+$BIN -d tests/args/
+
+print_test_name
+$BIN tests/args/file.ju
