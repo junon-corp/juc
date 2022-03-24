2288f241164b58f38ede32c21bb6e57c87ac133f
antoninhrlt <antoninhrlt1@gmail.com>
2022-03-24 19:05:08 +0100

    New tests

--- tests/compilation/test.sh
+++ tests/compilation/test.sh
@@ -3,5 +3,9 @@
 source tests/test.sh
 
 print_test_name
-$BIN -d tests/compilation main.ju
+$BIN -d tests/compilation/main/ test.ju -o test_main
+./tests/compilation/main/test_main
 
+print_test_name
+$BIN -d tests/compilation/vars/ test.ju -o test_vars
+./tests/compilation/vars/test_vars
