a7c7348aaeb010ce90018ea84eb2e99ba9b843ca
antoninhrlt <antoninhrlt1@gmail.com>
2022-04-04 09:35:46 +0200

    Update tests

--- tests/compilation/test.sh
+++ tests/compilation/test.sh
@@ -5,7 +5,9 @@ source tests/test.sh
 print_test_name
 $BIN -d tests/compilation/main/ test.ju -o test_main
 ./tests/compilation/main/test_main
+print_test_end
 
 print_test_name
 $BIN -d tests/compilation/vars/ test.ju -o test_vars
-valgrind ./tests/compilation/vars/test_vars
+./tests/compilation/vars/test_vars
+print_test_end $?
--- tests/test.sh
+++ tests/test.sh
@@ -1,3 +1,4 @@
+echo -e "\n<--------------->\n"
 if ! cargo build
 then
     exit 1
@@ -9,4 +10,14 @@ I=1
 function print_test_name() {
     echo "---------------> Test" $I
     ((I++))
+}
+
+function print_test_end() {
+    RETURNED_VALUE=$1
+    if [[ $RETURNED_VALUE == "" ]]
+    then
+        RETURNED_VALUE="nothing"
+    fi
+
+    echo "Test" $(expr $I - 1) "returned" $RETURNED_VALUE "<---------------" 
 }
