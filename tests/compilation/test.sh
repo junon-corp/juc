0dbbe4ad048f68de0e17f79cd0f5f6d037bbe7cd
antoninhrlt <antoninhrlt1@gmail.com>
2022-04-06 11:04:57 +0200

    Update + check tests

--- tests/compilation/test.sh
+++ tests/compilation/test.sh
@@ -5,9 +5,13 @@ source tests/test.sh
 print_test_name
 $BIN -d tests/compilation/main/ test.ju -o test_main
 ./tests/compilation/main/test_main
-print_test_end
+print_test_end $?
 
 print_test_name
 $BIN -d tests/compilation/vars/ test.ju -o test_vars
 ./tests/compilation/vars/test_vars
+print_test_end $?
+
+print_test_name
+$BIN -d tests/compilation/check/ test.ju -o test_check
 print_test_end $?
