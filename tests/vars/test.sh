5b50d3107fa8307a6271f4884653be932195ea6f
antoninhrlt <antoninhrlt1@gmail.com>
2022-04-25 00:12:02 +0200

    Update files for fixed bug on jup

--- tests/vars/test.sh
+++ tests/vars/test.sh
@@ -3,6 +3,6 @@
 source tests/test.sh
 
 print_test_name
-$BIN -d tests/compilation/vars/ test.ju -o test_vars
-./tests/compilation/vars/test_vars
+$BIN -d tests/vars/ test.ju -o test_vars
+./tests/vars/test_vars
 print_test_end $?
