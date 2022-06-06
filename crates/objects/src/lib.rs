cc2ba767ce34408b8fac063feb60fbc33acbbc25
antoninhrlt <antoninhrlt1@gmail.com>
2022-06-06 16:56:03 +0200

    Remove objects because jup

--- crates/objects/src/lib.rs
+++
@@ -1,8 +0,0 @@
-// This file is part of "juc"
-// Under the MIT License
-// Copyright (c) Junon, Antonin Hérault
-
-pub mod function;
-pub mod params;
-pub mod type_;
-pub mod variable;
--- crates/objects/src/params.rs
+++
@@ -1,7 +0,0 @@
-// This file is part of "juc"
-// Under the MIT License
-// Copyright (c) Junon, Antonin Hérault
-
-use crate::variable::Variable;
-
-pub type Params = Vec<Variable>;
