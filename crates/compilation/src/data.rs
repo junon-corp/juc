c359185ca90c9c2c1b935e8e5af29478aa2181dd
antoninhrlt <antoninhrlt1@gmail.com>
2022-07-21 23:49:11 +0200

    Seperate data structures for the different elements

--- crates/compilation/src/data.rs
+++ crates/compilation/src/data.rs
@@ -9,30 +9,47 @@ use jup::lang::elements::{
     variable::Variable 
 };
 use x64asm::formatter::Formatter;
-
 use strings::manager::StringsManager;
 
 use crate::scope::Scope;
 
-/// Important information given to the compiler
+//! All these structures will be used for the compilers
+//! These functions should be implemented :
+//! ```
+//! fn data(&mut self) -> &mut CompilerData;
+//! fn tools(&mut self) -> &mut CompilerTools;
+//! fn code_data(&mut self) -> &mut CompilerCodeData;
+//! fn stacks_data(&mut self) -> &mut CompilerStacksData;
+//! ```
+
+/// Some useful variables for the compiler
 pub struct CompilerData {
     pub is_library: bool,
-
+    
     pub sources: Vec<String>,
     pub options: Dict<String, String>,
+}
 
+/// Some tools used by the compilers
+pub struct CompilerTools {
     pub strings_manager: StringsManager,
-
     pub asm_formatter: Formatter,
+}
 
+/// Some useful variables for code walking
+pub struct CompilerCodeData {
     pub current_source: String,
     pub current_parsed: Vec<Element>,
-    pub current_scope: Scope,
-
+    pub scope: Scope,
+    
     pub next_element: Element,
+    /// Should the next element be skipped ?
     pub is_skip_next: bool,
+}
 
-    //                        id
+/// Some useful variables for the Assembly stacks
+pub struct CompilerStacksData {
+    /// First one is the variable's id
     pub variable_stack: Dict<String, Variable>,
     pub i_variable_stack: usize,
 
